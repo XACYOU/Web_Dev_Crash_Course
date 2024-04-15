3867f8588bfdc22217513f819c52bcf64bc7e04c
Saurabh Negi <xacyou20@gmail.com>
2024-04-15 21:32:38 +0530

    added me product page

---
+++ Project-1/script/men_product.js
@@ -0,0 +1,206 @@
+let data=[
+    {
+        imageURL: "https://images.asos-media.com/products/asos-design-oversized-tank-in-charcoal/205875115-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "ASOS DESIGN oversized tank in charcoal",
+        productPrice: 13.00,
+        category: "Shirt"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/asos-design-muscle-fit-t-shirt-with-crew-neck-in-black/203291724-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "ASOS DESIGN muscle fit t-shirt with crew neck in black",
+        productPrice: 8.50,
+        category: "T-Shirt"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/asos-design-6mm-glass-faux-pearl-beaded-bracelet-in-white/201876568-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "ASOS DESIGN 6mm glass faux pearl beaded bracelet in white",
+        productPrice: 9.50,
+        category: "Bracelet"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/asos-design-slim-regular-length-denim-short-in-black/205574922-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "ASOS DESIGN slim regular length denim short in black",
+        productPrice: 21.00,
+        category: "Joggers"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/topman-oversized-fit-sleeveless-t-shirt-in-stone/205600977-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "Topman oversized fit sleeveless t-shirt in stone",
+        productPrice: 15.00,
+        category: "T-Shirt"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/clarks-torhill-hi-boots-in-black-suede/205758664-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "Clarks Torhill hi boots in black suede",
+        productPrice: 116,
+        category: "Shoes"
+    },
+    {
+        imageURL:"https://images.asos-media.com/products/asos-design-nylon-tote-bag-in-khaki/205117194-2?$n_320w$&wid=317&fit=constrain",
+        productTitle:"ASOS DESIGN nylon tote bag in khaki",
+        productPrice: 27.00,
+        category: "Bag"
+    },
+    {
+        imageURL:"https://images.asos-media.com/products/reclaimed-vintage-90s-baggy-dad-in-ecru/205321616-2?$n_320w$&wid=317&fit=constrain",
+        productTitle:"Reclaimed Vintage 90s baggy dad in ecru",
+        productPrice: 35.99,
+        category: "Shirt"
+    },
+    {
+        imageURL:"https://images.asos-media.com/products/calvin-klein-jeans-90s-straight-leg-jeans-in-light-blue-wash/205437098-2?$n_320w$&wid=317&fit=constrain",
+        productTitle:"Calvin Klein Jeans 90's straight leg jeans in light blue wash",
+        productPrice: 110.00,
+        category: "Jeans"
+    },
+    {
+        imageURL:"https://images.asos-media.com/products/clarks-originals-wallabee-shoes-in-cola-suede/202778119-2?$n_320w$&wid=317&fit=constrain",
+        productTitle:"Clarks Originals wallabee shoes in cola suede",
+        productPrice: 137.00,
+        category: "Shoes"
+    },
+    {
+        imageURL:"https://images.asos-media.com/products/new-look-long-sleeve-oxford-shirt-in-navy/205456097-2?$n_320w$&wid=317&fit=constrain",
+        productTitle:"New Look long sleeve oxford shirt in navy",
+        productPrice: 18.99,
+        category: "Shirt"
+    },
+    {
+        imageURL:"https://images.asos-media.com/products/asos-design-slim-fit-oxford-shirt-in-white/205341872-2?$n_320w$&wid=317&fit=constrain",
+        productTitle:"ASOS DESIGN slim fit oxford shirt in white",
+        productPrice: 21.00,
+        category: "Shirt"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/dkny-logo-slider-slippers-in-black/202805845-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "DKNY logo slider slippers in black",
+        productPrice: 15.00,
+        category: "Slippers"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/asos-design-relaxed-fit-viscose-shirt-with-low-revere-collar-in-black/203983935-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "ASOS DESIGN relaxed fit viscose shirt with low revere collar in black",
+        productPrice: 24.00,
+        category: "Shirt"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/pullbear-cargo-trouser-in-sand/205195687-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "Pull&Bear cargo trouser in sand",
+        productPrice: 32.00,
+        category: "Trousers"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/only-sons-elasticated-waist-tapered-cropped-cord-trousers-in-dark-green/203563537-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "ONLY & SONS elasticated waist tapered cropped cord trousers in dark green",
+        productPrice: 40.00,
+        category: "Trousers"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/reclaimed-vintage-cropped-relax-trouser-in-black/204721372-1-black?$n_750w$&wid=714&fit=constrain",
+        productTitle: "Reclaimed Vintage cropped relax trouser in black",
+        productPrice: 25.99,
+        category: "Trousers"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/asos-design-chunky-trainers-with-monochrome-and-metallic-panelling/202802561-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "ASOS DESIGN chunky trainers with monochrome and metallic panelling",
+        productPrice: 42.50,
+        category: "Shoes"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/puma-teveris-nitro-trainers-in-white-and-green/204964427-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "Puma Teveris Nitro trainers in white and green",
+        productPrice: 58.50,
+        category: "Shoes"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/asos-design-heavyweight-oversized-hoodie-in-black/204932310-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "ASOS DESIGN heavyweight oversized hoodie in black",
+        productPrice: 34.00,
+        category: "Hoodie"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/asos-design-muscle-rib-vest-in-navy/205483290-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "ASOS DESIGN muscle rib vest in navy",
+        productPrice: 8.50,
+        category: "Vest"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/asos-design-sweatshirt-with-half-zip-in-black/204933657-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "ASOS DESIGN slim regular length denim short in black",
+        productPrice: 21.00,
+        category: "Sweatshirt"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/tommy-jeans-linen-blend-camp-shirt-in-white/205848049-1-white?$n_320w$&wid=317&fit=constrain",
+        productTitle: "Tommy Jeans linen blend camp shirt in white",
+        productPrice: 55.00,
+        category: "Shirt"
+    },
+    {
+        imageURL: "https://images.asos-media.com/products/topman-5-pack-classic-t-shirt-in-black-ecru-brown-blue-and-grey/205988146-2?$n_320w$&wid=317&fit=constrain",
+        productTitle: "Topman 5 pack classic t-shirt in black, ecru, brown, blue and grey",
+        productPrice: 43.00,
+        category: "T-Shirt"
+    }
+]
+let productCartStored=[];
+
+let prodCont=document.querySelector("#prod-cont");
+
+let saveData=()=>{
+    localStorage.setItem("products", JSON.stringify(productCartStored));
+    // console.log(productCartStored);
+}
+
+let oLoad=()=>{
+    let stored=JSON.parse(localStorage.getItem("products"));
+    if(stored.length){
+        productCartStored=stored;
+    }
+}
+
+oLoad();
+
+data.forEach((ele, i)=>{
+    ele.id=i+1;
+    ele.quantity=1;
+
+    let prodCard=document.createElement("div");
+    prodCard.id="prod-card";
+
+    let prodImg=document.createElement("img");
+    prodImg.id="prod-img";
+    prodImg.src=ele.imageURL
+
+    let prodTitle=document.createElement("p");
+    prodTitle.id="prod-title";
+    prodTitle.innerText=ele.productTitle;
+
+    let catPri=document.createElement("div");
+    catPri.id="cat-pri";
+
+    let prodCategory=document.createElement("p");
+    prodCategory.id="prod-category";
+    prodCategory.innerText=`${ele.category}`;
+
+    let prodPrice=document.createElement("p");
+    prodPrice.id="prod-price";
+    prodPrice.innerText=`$${ele.productPrice}`;
+
+    let prodCartBtn=document.createElement("button");
+    prodCartBtn.id="prod-cart-btn";
+    prodCartBtn.innerText="Add to cart";
+
+
+    catPri.append(prodPrice,prodCategory);
+    prodCard.append(prodImg,prodTitle,catPri,prodCartBtn);
+    prodCont.append(prodCard)
+
+
+    prodCartBtn.addEventListener("click",()=>{
+
+        productCartStored.push(ele);
+        saveData();
+    })
+})
